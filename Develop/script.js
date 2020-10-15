828e9c58b098e95b2d725da6335a827526b05e88
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-10-15 15:40:43 -0400

    before i mess everything up

--- Develop/script.js
+++ Develop/script.js
@@ -4,15 +4,15 @@ var generateBtn = document.querySelector("#generate");
 var upperArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
 ];
 var lowerArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var numericArray =  ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialCharacterArray = ['@','%','+',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+var numericArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; 
+var specialCharacterArray =  ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 
 function getPasswordOptions(){
-  var getUpperCase = window.confirm("would you like upper case letters in your password?");
-  var getLowerCase = window.confirm("Would you like lower case letters in your password?");
-  var getNumeric = window.confirm("Would you like numbers in your password?");
-  var getSpecialCharacter = window.confirm("Would you like special characters in your password?");
-  var chooseLength = window.prompt("Please choose a length between 8 and 128 characters.");
+  var getUpperCase = window.confirm("would you like upper case letters in your password?")
+  var getLowerCase = window.confirm("Would you like lower case letters in your password?")
+  var getNumeric = window.confirm("Would you like numbers in your password?")
+  var getSpecialCharacter = window.confirm("Would you like special characters in your password?")
+  var chooseLength = window.prompt("Please choose a length between 8 and 128 characters.")
 
   var passwordOptions = {
     upperCase: getUpperCase,
